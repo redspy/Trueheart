bce9a8de7549328b8fa112f9eeb36a79b6fa662b
redspy <redspy@lycos.co.kr>
2017-02-07 18:44:08 +0900

    detail Page 에서 Font Size 변경 가능

--- src/pages/example-detail/example-detail.ts
+++ src/pages/example-detail/example-detail.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 /*
   Generated class for the ExampleDetail page.
@@ -13,13 +14,33 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ExampleDetailPage {
   selectedItem: any;
+  fontSize: any;
+  fontSizeInteger: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
     this.selectedItem = navParams.get('item');
+    this.fontSizeInteger = 1.4;
+    this.fontSize = this.fontSizeInteger + 'rem';
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ExampleDetailPage');
   }
+  showToast(messageString: string) {
+    let toast = this.toastCtrl.create({
+      message: messageString,
+      duration: 2000,
+      position: 'bottom'
+    });
+    toast.present(toast);
+  }
 
+  changeFontSize(option: string) {
+    if (option == 'up') {
+      this.fontSizeInteger = this.fontSizeInteger + 0.1;
+    } else {
+      this.fontSizeInteger = this.fontSizeInteger - 0.1;
+    }
+    this.fontSize = this.fontSizeInteger + 'rem';
+  }
 }
