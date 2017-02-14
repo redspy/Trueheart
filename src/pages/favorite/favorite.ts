cb31a9049d0d2d8c46ab65e47ac6e462e92858d8
redspy <redspy@lycos.co.kr>
2017-02-14 09:35:28 +0900

    책갈피 페이지는 만들었는데 누르면 어떻게해야하지

--- src/pages/favorite/favorite.ts
+++ src/pages/favorite/favorite.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Nav } from 'ionic-angular';
 import { NavController, NavParams } from 'ionic-angular';
-import { ExampleDetailPage } from '../../pages/example-detail/example-detail';
-import data from '../../assets/data/mydata.json';
+import { DataService } from '../../services/dataService';
 import { ConfigService } from '../../services/configService';
 /*
   Generated class for the Favorite page.
@@ -14,61 +14,26 @@ import { ConfigService } from '../../services/configService';
   templateUrl: 'favorite.html'
 })
 export class FavoritePage {
+  @ViewChild(Nav) nav: Nav;
+  titleList: any;
 
-  selectedItem: any;
-  items: Array<{ id: string, title: string, body: string, image: string }>;
-  images: string[];
-  configValue: ConfigService;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public configService: ConfigService) {
-    this.configValue = configService;
-    this.itemAssign();
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              private dataService: DataService,
+              private configService: ConfigService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FavoritePage');
-  }
-  itemAssign() {
-    this.items = [];
-    this.images = ['assets/img/pocket-watch-2036304_1920.jpg',
-      'assets/img/european-eagle-owl-2010346_1280.jpg',
-      'assets/img/orange-1995056_1280.jpg',
-      'assets/img/white-tailed-eagle-2015098_640.jpg',
-      'assets/img/skyscraper-1893201_640.jpg',
-      'assets/img/sunrise-1756274_640.jpg',
-      'assets/img/bullion-1744773_640.jpg',
-      'assets/img/theater-1713816_640.jpg',
-      'assets/img/sun-1654458_640.jpg',
-      'assets/img/ring-1692713_640.jpg',
-      'assets/img/wallpaper-1492818_640.jpg',
-      'assets/img/children-1639420_640.jpg',
-      'assets/img/background-1524540_640.jpg',
-      'assets/img/clock-1516967_640.png',
-      'assets/img/piano-keys-1090984_640.jpg',
-      'assets/img/clock-1392328_640.jpg',
-      'assets/img/flame-1363003_640.jpg',
-      'assets/img/snail-83672_640.jpg',
-      'assets/img/abstract-1231889_640.jpg',
-      'assets/img/fractal-1128622_640.jpg',
-      'assets/img/landscape-982178_640.jpg',
-      'assets/img/glass-1206584_640.jpg',
-      'assets/img/tree-51358_640.png',
-      'assets/img/panorama-1993645_1280.jpg'];
-    for (let i = 0; i < data.poems.length; i++) {
-      if (this.configValue.getFavorite(data.poems[i].id)) {
-        this.items.push({
-          id: data.poems[i].id,
-          title: data.poems[i].title,
-          body: data.poems[i].body,
-          image: this.images[Math.floor(Math.random() * this.images.length)]
-        });
+    this.titleList = [];
+    for (let i=0; i<this.dataService.getMaxCount(); i++) {
+      if (this.configService.getFavorite(this.dataService.getIDbyIndex(i))) {
+        this.titleList.push(this.dataService.getTitlebyIndex(i));
       }
     }
   }
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ExampleDetailPage, {
-      item: item
-    });
+
+  selectPoem(page) {
+
   }
+
 }
